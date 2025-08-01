411394be5661fe33b1807639d46b3c8d59155201
navys00 <gjxnf93263@mail.ru>
2025-08-01 19:38:26 +0300

    add paddleOCR reader

--- python/test_ocr.py
+++ python/test_ocr.py
@@ -1,6 +1,25 @@
-import sys
-from pathlib import Path
+# import sys
+# from pathlib import Path
+# from paddleocr import PaddleOCR
+# from paddleocr import PPStructureV3
+# ocr = PaddleOCR(use_angle_cls=True, lang='ru',show_log=False)
+# ocr = PaddleOCR()
+# structure = PPStructureV3(use_doc_orientation_classify=True)
+# res = ocr.ocr('test.png', cls=True)
+# print(res)
 from paddleocr import PaddleOCR
-ocr = PaddleOCR(use_angle_cls=True, lang='ru',show_log=False)
-res = ocr.ocr('test.png', cls=True)
-print(res)
+ocr = PaddleOCR(
+    lang='ru',
+    use_doc_orientation_classify=False,
+    use_doc_unwarping=False,
+    use_textline_orientation=False)
+
+# Run OCR inference on a sample image 
+result = ocr.predict(
+    input="./photo_1754050117342.jpg")
+
+# Visualize the results and save the JSON results
+for res in result:
+    res.print()
+    res.save_to_img("output")
+    res.save_to_json("output")
